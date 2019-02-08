b2234e1e46e21913839c50099b2948510f76edc3
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-08 07:56:16 +0300

    add example convert format date

--- examples/examples-date.php
+++ examples/examples-date.php
@@ -7,11 +7,17 @@ use cse\helpers\Date;
 // Example: get time
 // 01.01.2018 => 1514754000
 var_dump(Date::getTime('01.01.2018'));
+echo PHP_EOL;
 
 // Example: to Format
 // 2018-01-01 => 01.01.2018
 var_dump(Date::toFormat('2018-01-01'));
+// Example: to Format change default format
+// 2018-01-01 => 01/01/2018
+var_dump(Date::toFormat('2018-01-01', 'd/m/Y'));
+echo PHP_EOL;
 
 // Example: to SQL
 // 01.01.2018 => 2018-01-01
 var_dump(Date::toSQL('01.01.2018'));
+echo PHP_EOL;
