2c765693130cb695da29943550fd52b100948075
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-08 07:35:41 +0300

    add example convert date to SQL

--- examples/examples-date.php
+++ examples/examples-date.php
@@ -11,3 +11,7 @@ var_dump(Date::getTime('01.01.2018'));
 // Example: to Format
 // 2018-01-01 => 01.01.2018
 var_dump(Date::toFormat('2018-01-01'));
+
+// Example: to SQL
+// 01.01.2018 => 2018-01-01
+var_dump(Date::toSQL('01.01.2018'));
