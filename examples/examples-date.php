35c53fa4152225d997e574947d18ce1b23d52923
cs-eliseev <a.k.eliseev@gmail.com>
2019-02-09 15:03:47 +0300

    add example default current date

--- examples/examples-date.php
+++ examples/examples-date.php
@@ -7,6 +7,10 @@ use cse\helpers\Date;
 // Example: get time
 // 01.01.2018 => 1514754000
 var_dump(Date::getTime('01.01.2018'));
+// Example: current date
+var_dump(Date::getTime());
+// Example: relative time
+var_dump(Date::getTime('+1 week 2 days 4 hours 2 seconds'));
 echo PHP_EOL;
 
 // Example: to Format
@@ -15,11 +19,19 @@ var_dump(Date::toFormat('2018-01-01'));
 // Example: to Format change default format
 // 2018-01-01 => 01/01/2018
 var_dump(Date::toFormat('2018-01-01', 'd/m/Y'));
+// Example: current date
+var_dump(Date::toFormat());
+// Example: relative time
+var_dump(Date::toFormat('+1 week 2 days 4 hours 2 seconds'));
 echo PHP_EOL;
 
 // Example: to SQL
 // 01.01.2018 => 2018-01-01
 var_dump(Date::toSQL('01.01.2018'));
+// Example: current date
+var_dump(Date::toSQL());
+// Example: relative time
+var_dump(Date::toSQL('+1 week 2 days 4 hours 2 seconds'));
 echo PHP_EOL;
 
 // Example: diff
@@ -27,10 +39,15 @@ echo PHP_EOL;
 var_dump(Date::diff('2018-07-01', '2018-07-01'));
 // 2
 var_dump(Date::diff('2018-07-01 02:00:00', '01.01.2018 00:00:00', '%h'));
+// Example: timestamp
 // 2
 var_dump(Date::diff(strtotime('2018-09-02'), strtotime('2018-07-01'), '%m'));
 // 0
 var_dump(Date::diff('31.01.2018', strtotime('2018-01-02'), '%d/%m'));
+// Example: current date
+var_dump(Date::diff('31.01.2018'));
+// Example: relative time
+var_dump(Date::diff('31.01.2018', '+1 week'));
 echo PHP_EOL;
 
 // Example: current
@@ -40,12 +57,20 @@ var_dump(Date::current(Date::FORMAT_SQL));
 echo PHP_EOL;
 
 // Example: extreme mouth date
-var_dump(Date::extremeMouthDate());
+var_dump(Date::extremeMouthDate('11.01.2018'));
 // Example: first date mouth
-var_dump(Date::extremeMouthDate('now', 'Y-m-01'));
+var_dump(Date::extremeMouthDate(strtotime('28.02.2018'), 'Y-m-01'));
+// Example: current date
+var_dump(Date::extremeMouthDate());
+// Example: relative time
+var_dump(Date::extremeMouthDate('+1 week'));
 echo PHP_EOL;
 
 // Example: get quarter
 // 31.12.2018 23:59:59 => 4
 var_dump(Date::getQuarter('31.12.2018 23:59:59'));
+// Example: current date
+var_dump(Date::getQuarter());
+// Example: relative time
+var_dump(Date::getQuarter('+1 week'));
 echo PHP_EOL;
